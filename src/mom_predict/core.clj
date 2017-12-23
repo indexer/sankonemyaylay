9decaa857e1517e87f57f9fbc4180f8268e3a871
indexer <yemonkyaw007@gmail.com>
2017-12-23 11:18:25 +0800

    long sentenct to short

--- src/mom_predict/core.clj
+++ src/mom_predict/core.clj
@@ -1,22 +1,32 @@
 (ns mom-predict.core
   (:gen-class)
   (require [clojure.data.csv :as csv]
-           [clojure.java.io :as io]))
+           [clojure.java.io :as io]
+           [clj-time.core :as ti]))
 
 (defn readcsv []
   (with-open [reader (io/reader "data.csv")]
     (doall
      (csv/read-csv reader))))
 
+(defn selectstatus[statusvalue,x]
+  (println statusvalue
+           (format "%3f"
+                   (float ( / (count (get x statusvalue)) (count x))))))
 
-(defn groupbydate [x]
-  (group-by :date (into [] (for [record (x)
-                                 :while (seq (first record))]
-                             (zipmap [:no :name :date :status] record)))))
+(defn pairwithkey[y]
+  (zipmap [:id :finnumber
+           :passtype :hashnumber :status :date :zg] y))
+
+
+(defn mapwithkey [x]
+  (selectstatus "Pending",
+                (group-by :status (into [] (for [record (x) :while (seq (first record))]
+                                             (pairwithkey record))))))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (groupbydate readcsv))
+  (mapwithkey readcsv))
 
