a30edb915a936be891dd7684f4f786e7abb9e463
indexer <yemonkyaw007@gmail.com>
2017-12-24 14:15:16 +0800

    update data and status

--- src/mom_predict/core.clj
+++ src/mom_predict/core.clj
@@ -2,30 +2,46 @@
   (:gen-class)
   (require [clojure.data.csv :as csv]
            [clojure.java.io :as io]
-           [clj-time.core :as ti]))
+           [clj-time.core :as ti]
+           [clj-time.format :as f]
+           ))
 
 (defn readcsv []
   (with-open [reader (io/reader "data.csv")]
     (doall
      (csv/read-csv reader))))
 
+(def month-formatter (f/formatter "dd/MM/yyyy"))
 
-(defn selectstatus[statusvalue,filter,total]  
-  (println statusvalue
-           (format "%3f"
-                   (* (float ( / (count (get filter statusvalue)) (count (total))))100))))
+(defn showpercentage[status,value,total]
+  (println "Total" status  (count (get value status)))
+  (format "%3f"
+          (* (float ( / (count (get value status)) (count (total))))100)))
+
+
+(defn selectMonth[statusvalue,filter,total]
+  (println "Month of year "  statusvalue
+           "Total Apply" (count (get filter statusvalue)))
+  (println "Total Percentage"
+           (showpercentage "Approved",
+                           (group-by :status
+                                     (for [current (get filter statusvalue)
+                                           :while (seq (first current))]
+                                       current)),total)))
+
 
 
 (defn pairwithkey[row]
   (zipmap [:id :finnumber
-           :passtype :hashnumber :status :date :zg] row))
+           :passtype :hashnumber :status :date :zg]
+          (assoc row 5 (ti/month
+                        (f/parse month-formatter (get row 5))))))
 
 
-(defn mapwithkey [totalcsv]
-  (selectstatus "Approved",
-                (group-by :status
-                          (into [] (for [row (totalcsv) :while (seq (first row))]
-                                     (pairwithkey row)))),totalcsv))
+(defn mapwithkey [totalcsv] 
+  (selectMonth 11, (group-by :date
+                             (into [] (for [row (totalcsv) :while (seq (first row))]
+                                        (pairwithkey row)))),totalcsv))
 
 
 (defn -main
